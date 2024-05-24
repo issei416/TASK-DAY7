88cc7d82642a78267733d67eb552bc9a57ee54ff
issei416 <mukeshkj2912@gmail.com>
2024-05-24 16:00:06 +0530

    TASK DAY 7

---
+++ README.md
@@ -0,0 +1,11 @@
+# TASK DAY 7
+
+Today's task involves using array functions to get data from the API called rest countries
+
+functions to solve:
+* get countries in asia region
+* get countries with population < 2 lakhs
+* print nmae,capital and flag of each country in rest countries
+* print total population of countries using reduce method
+* get countries using US dollers as currency
+
--- script/script.js
+++ script/script.js
@@ -3,31 +3,31 @@ function reqListener() {
     console.log(countryArray);
     console.log("COUNTRIES IN ASIA")
     console.log(countryArray.filter(x => {
-      if(x.region==="Asia"){
+      if(x.region==="Asia"){  //select countries if the region is asia
         console.log(`${x.name.common}: `,x.region);
         return x;
       }
     } ));
     console.log("COUNTRIES WITH POPULATION LESS THAT 2 LAKHS");
     console.log(countryArray.filter(x => {
-      if(x.population<200000){
+      if(x.population<200000){ // select countries only if population is less than 2 lakhs
       console.log(`${x.name.common} :`,x.population);
       return x;
       }
     }));
     console.log("NAME, CAPITAL, FLAG, USING forEach")
     countryArray.forEach(x => {
-      console.log(`name : ${x.name.common}, \ncapital : ${x.capital}, \nflag : ${x.flag}`)
+      console.log(`name : ${x.name.common}, \ncapital : ${x.capital}, \nflag : ${x.flag}`) //prints name,capital,flag for all countries
       return x;
     });
     console.log("TOTAL POPULATION")
     console.log(countryArray.reduce((total,country) => {
-      return total+country.population
+      return total+country.population // prints total population using reduce method
     },0));
     console.log("COUNTRIES THAT USE USD")
     countryArray.forEach(x => {
-      let c = x.currencies;
-      if (c && Object.keys(c).includes('USD')) {
+      let c = x.currencies; // get keys of the currencies property 
+      if (c && Object.keys(c).includes('USD')) { // only if property is not undefined or null and only it includes "USD" print the name
         console.log(x.name.common);
     }
     });
